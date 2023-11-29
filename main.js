f9239195e2e01ac56bab6857ff7458369ec01064
weihong <weihong2398@gmail.com>
2023-11-29 13:16:27 -0500

    kkk

---
+++ main.js
@@ -0,0 +1,34 @@
+// function greet(name){
+//     const greetPromise = new Promise(function (resolve, rejected){
+//         resolve(`hello ${name}`);
+//     });
+//     return greetPromise;
+// }
+// //promise must be handled after we recieve them
+// const Katherine = greet("Nuggies");
+// nuggies.then((result)=>
+//  { console.log(result);
+// });
+//Rest API
+const URL = `https://api.quotable.io/random`;
+
+async function getData(URL){
+    try {
+
+ const response = await fetch(URL);
+    console.log(response);
+    if(response.status != 200){
+        throw new Error(response.statusText);
+    }
+    //take response from serevr and converts it to JSON
+    const data = await response.json();
+    document.querySelector("h1").textContent = data.content
+    document.querySelector("h2").textContent = data.author
+    }catch(error){ 
+        document.querySelector("h1").textContent = error;
+        document.querySelector("h1").textContent = "Please search";
+
+    }
+    
+}
+getData(URL);
