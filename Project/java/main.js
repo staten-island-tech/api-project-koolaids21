8f702c5a4757ffc3825e455556dd4b7ad606b947
koolaids21 <weihong2398@gmail.com>
2023-12-19 23:15:12 -0500

    zzzzzzz

--- Project/java/main.js
+++ Project/java/main.js
@@ -24,60 +24,61 @@
 //     //take response from serevr and converts it to JSON
 //     const data = await response.json();
 //     document.querySelector("h1").textContent = data.agent
-//     }catch(error){ 
+//     }catch(error){
 //         document.querySelector("h1").textContent = error;
 //         document.querySelector("h1").textContent = "Please search";
 
 //     }
-    
+
 // }
 // getData(URL);
-
-
 
 // console.log(fetch('https://valorant-api.com/v1/agents/23')
 // .then(res => res.json())
 // .then(data => console.log(data))
 // )
 
+const DOMSELECTORS = {
+  agent: document.getElementById("agent"),
+};
+
 const apiURL = "https://valorant-api.com/v1/agents";
 
 console.log(fetch(apiURL));
+
 fetch(apiURL)
-.then((response) => response.json())
-.then((data) => console.log(data));
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    getData(data.data);
+  })
+  .catch((error) => console.error("error fetching data do someelse:", error));
 
-
-const DOMSELECTORs = {
-agent: document.getElementById("agent"),
-
-}
-
-
-
-
-async function getData(apiURL){
-    try {
+async function getData(agents) {
+  try {
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data.data);
-    return data;
-    } catch (error) {
-        console.log(error);
-    }
+    insertCard(data.data);
+  } catch (error) {
+    console.error("error processing data do someelse:", error);
+  }
 }
-getData(apiURL);
 
-const apiResponseDOM = document.getElementById("agent");
-const putintoHTML = async () => {
-    const thing = await fetch(apiURL)
-    apiResponseDOM.textContent = `Name: ${thing.uuid}`;
-    DOMSELECTORs.agent.insertAdjacentHTML(
-        "beforeend",
-       putintoHTML()
-
+function insertCard(arr) {
+  arr.forEach((agent) => {
+    DOMSELECTORS.agent.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <h2 class="name">${agent.displayName}</h2>
+      </div>`
     );
-}; 
-putintoHTML()
-
-
+  });
+}
+// const apiResponseDOM = document.getElementById("agent");
+// const putintoHTML = async () => {
+//   const thing = await fetch(apiURL);
+//   apiResponseDOM.textContent = `Name: ${thing.uuid}`;
+//   DOMSELECTORs.agent.insertAdjacentHTML("beforeend", putintoHTML());
+// };
+// putintoHTML();
